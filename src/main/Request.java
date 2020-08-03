d8a7330c3f8d3f76948d179cb69327b296b4a8ef
Nathan Safir <47368416+nssafir@users.noreply.github.com>
2020-08-03 15:22:55 -0500

    Update Request.java

--- src/main/Request.java
+++ src/main/Request.java
@@ -1,9 +1,11 @@
 public class Request {
-  Video video;
-  int number;
+  public Video video;
+  public int number;
+  public Endpoint endpoint;
 
-  public Request(Video video, int number) {
+  public Request(Video video, int number, Endpoint endpoint) {
     this.video = video;
     this.number = number;
+    this.endpoint = endpoint;
   } 
 }
