82491db0967cdb1a799d1292f24d6091277fd38d
Nathan Safir <47368416+nssafir@users.noreply.github.com>
2020-08-03 15:21:27 -0500

    Update Endpoint.java

--- src/main/Endpoint.java
+++ src/main/Endpoint.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Endpoint {
   public List<Request> requests;
+  public Map<Cache, int> connections;
   
   public Endpoint() {
     requests = new ArrayList<>();
+    connections = new HashMap<>();
   }
 
   public void addRequest(Request r) {
     requests.add(r);
   }
+  
+  public void addConnection(Cache c, int latency) {
+    connections.put(c, latency);
 }
