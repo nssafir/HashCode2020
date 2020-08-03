b09489b14bba96c56142fd68910d14906203de5f
Nathan Safir <47368416+nssafir@users.noreply.github.com>
2020-08-03 15:36:03 -0500

    Update Endpoint.java

--- src/main/Endpoint.java
+++ src/main/Endpoint.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 public class Endpoint {
   public List<Request> requests;
   public Map<Cache, int> connections;
+  public int identifier;
   
-  public Endpoint() {
+  public Endpoint(int identifier) {
     requests = new ArrayList<>();
     connections = new HashMap<>();
+    this.identifier = identifier;
   }
 
   public void addRequest(Request r) {
